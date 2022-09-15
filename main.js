8d50ed20853fb5ce40f9b7c6cb4167151adc2c01
kuruu <111561391+zokuru@users.noreply.github.com>
2022-09-15 17:42:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,228 +2,230 @@ const functions = require("./functions.js");
 const fs = require("node:fs");
 
 class ZokuDB {
-  constructor(options) {
+    constructor(options) {
 
-    this.dbName = options["dbName"];
-    this.dbFolder = options["dbFolder"];
-    this.noBlankData = options["noBlankData"] ? (typeof options["noBlankData"] === "boolean" ? options["noBlankData"] : false) : false;
-    this.readable = options["readable"] ? (typeof options["readable"] === "boolean" ? true : false) : false;
-    this.message = {
-      "errors": {
-        "blankName": "Please give me a database name.",
-        "blankData": "Please give me a data.",
-        "blankNumber": "Please give me a number." 
-      }
-    };
+        this.dbName = options["dbName"];
+        this.dbFolder = options["dbFolder"];
+        this.noBlankData = options["noBlankData"] ? (typeof options["noBlankData"] === "boolean" ? options["noBlankData"] : false) : false;
+        this.readable = options["readable"] ? (typeof options["readable"] === "boolean" ? true : false) : false;
+        this.message = {
+            "errors": {
+                "blankName": "Please give me a database name.",
+                "blankData": "Please give me a data.",
+                "blankNumber": "Please give me a number."
+            }
+        };
 
-    functions.fetchFiles(this.dbFolder, this.dbName);
-  }
-  
-  set(db, data) {
-    functions.fetchFiles(this.dbFolder, this.dbName);
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+        functions.fetchFiles(this.dbFolder, this.dbName);
     }
-    if (!data) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    functions.set(db, data, content);
 
-    if (this.readable) {
-      fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content, null, 2));
-    } else {
-      fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content));
-    }
-    return this.get(db);
-  }
+    set(db, data) {
+        functions.fetchFiles(this.dbFolder, this.dbName);
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        if (!data) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        functions.set(db, data, content);
 
-  get(db) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+        if (this.readable) {
+            fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content, null, 2));
+        } else {
+            fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content));
+        }
+        return this.get(db);
     }
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    return functions.get(content, ...db.split("."));
-  }
 
-  fetch(db) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    get(db) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        return functions.get(content, ...db.split("."));
     }
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    return functions.get(content, ...db.split("."));
-  }
 
-  has(db) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    fetch(db) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        return functions.get(content, ...db.split("."));
     }
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    return functions.get(content, ...db.split(".")) ? true : false;
-  }
 
-  delete(db) {
-    functions.fetchFiles(this.dbFolder, this.dbName);
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    has(db) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        return functions.get(content, ...db.split(".")) ? true : false;
     }
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    if(!this.get(db)) {
-      return false;
-    }
-    functions.delete(content, db);
-    if(this.noBlankData === true) {
-      functions.removeEmptyData(content);
-    }
-    if(this.readable) {
-      fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content, null, 2));
-    } else {
-      fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content));
-    }
-    return true;
-  }
 
-  add(db, number) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    delete(db) {
+        functions.fetchFiles(this.dbFolder, this.dbName);
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        if (!this.get(db)) {
+            return false;
+        }
+        functions.delete(content, db);
+        if (this.noBlankData === true) {
+            functions.removeEmptyData(content);
+        }
+        if (this.readable) {
+            fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content, null, 2));
+        } else {
+            fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify(content));
+        }
+        return true;
     }
-    if(!number) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    if(isNaN(number)) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    this.set(db, Number(this.get(db) ? (isNaN(this.get(db)) ? Number(number) : this.get(db)+Number(number)) : Number(number)));
-    return this.get(db);
-  }
 
-  subtract(db, number) {
-    if(!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    add(db, number) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        if (!number) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        if (isNaN(number)) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        this.set(db, Number(this.get(db) ? (isNaN(this.get(db)) ? Number(number) : this.get(db) + Number(number)) : Number(number)));
+        return this.get(db);
     }
-    if(!number) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    if(isNaN(number)) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    if(this.get(db)-number <= 1) {
-      this.delete(db);
-      return (this.get(db) || 0)
-    }
-    if(!this.get(db)) {
-      this.delete(db);
-      return (this.get(db) || 0)
-    }
-    this.set(db, this.get(db) ? (this.get(db)-Number(number) <= 1 ? 1 : (isNaN(this.get(db)) ? 1 : this.get(db)-Number(number)) || 1) : 1);
-    return this.get(db);
-  }
 
-  push(db, data) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    subtract(db, number) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        if (!number) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        if (isNaN(number)) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        if (this.get(db) - number <= 1) {
+            this.delete(db);
+            return (this.get(db) || 0)
+        }
+        if (!this.get(db)) {
+            this.delete(db);
+            return (this.get(db) || 0)
+        }
+        this.set(db, this.get(db) ? (this.get(db) - Number(number) <= 1 ? 1 : (isNaN(this.get(db)) ? 1 : this.get(db) - Number(number)) || 1) : 1);
+        return this.get(db);
     }
-    if (!data) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    var arr = [];
-    if (this.get(db)) {
-      if(typeof this.get(db) !== "object") {
-        arr = [];
-      } else {
-        arr = this.get(db);
-      }
-    }
-    arr.push(data);
-    this.set(db, arr);
-    return this.get(db);
-  }
 
-  unpush(db, data) {
-    if (!db) {
-      throw new TypeError(this.message["errors"]["blankName"]);
+    push(db, data) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        if (!data) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        var arr = [];
+        if (this.get(db)) {
+            if (typeof this.get(db) !== "object") {
+                arr = [];
+            } else {
+                arr = this.get(db);
+            }
+        }
+        arr.push(data);
+        this.set(db, arr);
+        return this.get(db);
     }
-    if (!data) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    var arr = [];
-    if (this.get(db)) {
-      arr = this.get(db);
-    }
-    arr = arr.filter((x) => x !== data);
-    this.set(db, arr);
-    return this.get(db);
-  }
 
-  delByPriority(data, number) {
-    if (!data) {
-      throw new TypeError(this.message["errors"]["blankData"]);
+    unpush(db, data) {
+        if (!db) {
+            throw new TypeError(this.message["errors"]["blankName"]);
+        }
+        if (!data) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        var arr = [];
+        if (this.get(db)) {
+            arr = this.get(db);
+        }
+        arr = arr.filter((x) => x !== data);
+        this.set(db, arr);
+        return this.get(db);
     }
-    if (!number) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    if (isNaN(number)) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    if (!this.get(data) || this.get(data).length < 1) {
-      return false;
-    }
-    let content = this.get(data);
-    let neww = [];
-    if (typeof content !== "object") {
-      return false;
-    }
-    for (let a = 0; a < content.length; a++) {
-      if (a !== (number-1)) {
-        neww.push(content[`${a}`]);
-      }
-    }
-    this.set(data, neww);
-    return this.get(data);
-  }
 
-  setByPriority(data, value, number) {
-    if (!data) {
-      throw new TypeError(this.message["errors"]["blankData"]);
+    delByPriority(data, number) {
+        if (!data) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        if (!number) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        if (isNaN(number)) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        if (!this.get(data) || this.get(data).length < 1) {
+            return false;
+        }
+        let content = this.get(data);
+        let neww = [];
+        if (typeof content !== "object") {
+            return false;
+        }
+        for (let a = 0; a < content.length; a++) {
+            if (a !== (number - 1)) {
+                neww.push(content[`${a}`]);
+            }
+        }
+        this.set(data, neww);
+        return this.get(data);
     }
-    if (!value) {
-      throw new TypeError(this.message["errors"]["blankData"]);
-    }
-    if (!number) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    if (isNaN(number)) {
-      throw new TypeError(this.message["errors"]["blankNumber"]);
-    }
-    if (!this.get(data) || this.get(data).length < 1) {
-      return false;
-    }
-    let content = this.get(data);
-    let neww = [];
-    if (typeof content !== "object") {
-      return false;
-    }
-    for (let a = 0; a < content.length; a++) {
-      let val = content[`${a}`];
-      if(a === (number-1)) {
-        neww.push(value);
-      } else {
-        neww.push(val);
-      }
-    }
-    this.set(data, neww);
-    return this.get(data);
-  }
 
-  all() {
-    const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
-    return content;
-  }
+    setByPriority(data, value, number) {
+        if (!data) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        if (!value) {
+            throw new TypeError(this.message["errors"]["blankData"]);
+        }
+        if (!number) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        if (isNaN(number)) {
+            throw new TypeError(this.message["errors"]["blankNumber"]);
+        }
+        if (!this.get(data) || this.get(data).length < 1) {
+            return false;
+        }
+        let content = this.get(data);
+        let neww = [];
+        if (typeof content !== "object") {
+            return false;
+        }
+        for (let a = 0; a < content.length; a++) {
+            let val = content[`${a}`];
+            if (a === (number - 1)) {
+                neww.push(value);
+            } else {
+                neww.push(val);
+            }
+        }
+        this.set(data, neww);
+        return this.get(data);
+    }
 
-  deleteAll() {
-    fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify({}, null, 2));
-    return true;
-  }
+    all() {
+        const content = JSON.parse(fs.readFileSync(`./${this.dbFolder}/${this.dbName}.json`, "utf8"));
+        return content;
+    }
+
+    deleteAll() {
+        fs.writeFileSync(`./${this.dbFolder}/${this.dbName}.json`, JSON.stringify({}, null, 2));
+        return true;
+    }
 }
 
-module.exports = { ZokuDB };
+module.exports = {
+    ZokuDB
+};
